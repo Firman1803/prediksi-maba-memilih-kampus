8294d3c23ac71a8eab0f11664271db1fe1aea9c5
Firman1803 <firmanhidayahtullah@students.unnes.ac.id>
2025-06-18 08:15:51 +0700

    Update 1_dashboard.py

--- pages/1_dashboard.py
+++ pages/1_dashboard.py
@@ -3,24 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+df = pd.read_csv("dataset.csv", sep=";")
+df.columns = ['Akreditasi', 'Uang_Kuliah', 'Fasilitas', 'Pelayanan', 'Lokasi', 'Pilih_PTN']
+
 st.title("📊 Dashboard Data")
-
-# Load dataset
-df = pd.read_csv("dataset.csv")
-
-st.subheader("Preview Data")
 st.dataframe(df.head())
 
-st.subheader("Distribusi Akreditasi")
-st.bar_chart(df['Akreditasi'].value_counts())
+st.subheader("Distribusi Pilihan PTN")
+st.bar_chart(df['Pilih_PTN'].value_counts())
 
-st.subheader("Distribusi Kolom Numerik")
-num_cols = ['Uang_Kuliah', 'Fasilitas', 'Pelayanan', 'Lokasi']
-st.line_chart(df[num_cols])
-
-st.subheader("Korelasi Antar Fitur")
-corr = df[num_cols + ['Pilih_PTN']].corr()
+st.subheader("Korelasi")
+df['Pilih_PTN'] = df['Pilih_PTN'].map({'Ya': 1, 'Tidak': 0})
+corr = df.corr(numeric_only=True)
 fig, ax = plt.subplots()
 sns.heatmap(corr, annot=True, cmap="coolwarm", ax=ax)
 st.pyplot(fig)
-
