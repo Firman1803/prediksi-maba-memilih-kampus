25f71b0589201e9fe74a462a5f82623486a77b19
Firman1803 <firmanhidayahtullah@students.unnes.ac.id>
2025-06-18 07:53:09 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Prediksi PTN", layout="wide")
+st.title("🎓 Aplikasi Prediksi Pemilihan Perguruan Tinggi")
+st.markdown("Silakan pilih halaman di sidebar 👉 untuk melihat dashboard, performa model, atau mencoba prediksi.")
