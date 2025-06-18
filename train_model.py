f507417a2c81d8e4d48989dc5875588431a8ed53
Firman1803 <firmanhidayahtullah@students.unnes.ac.id>
2025-06-18 08:26:24 +0700

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+# Load dataset
+df = pd.read_csv("dataset.csv", sep=";")
+df.columns = ['Akreditasi', 'Uang_Kuliah', 'Fasilitas', 'Pelayanan', 'Lokasi', 'Pilih_PTN']
+df['Pilih_PTN'] = df['Pilih_PTN'].map({'Ya': 1, 'Tidak': 0})
+
+# Feature & Target
+X = df[['Akreditasi', 'Uang_Kuliah', 'Fasilitas', 'Pelayanan', 'Lokasi']]
+y = df['Pilih_PTN']
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Save model
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
